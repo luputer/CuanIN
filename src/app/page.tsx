73484c96e2fbf27e57764a15d832e45584666fd5
Asyaika <asyaika.zahrah05@gmail.com>
2026-04-08 11:23:57 +0800

    push ulang

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import Image from "next/image";
-import Link from "next/link";
 import Button from "~/components/ui/buttonlogin";
 import HeaderLandingPage from "~/components/layout/headerlandingpage";
 import StepsSection from "~/components/ui/stepsection";

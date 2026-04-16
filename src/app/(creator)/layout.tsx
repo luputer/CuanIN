aa1b27127f8a1a8fd5ddaf2c51411d8dba6ae4ca
luputer <m.saidibjm4@gmail.com>
2026-04-16 12:44:10 +0800

    style: :bug: fix layout bug
    fix scroll layout

--- src/app/(creator)/layout.tsx
+++ src/app/(creator)/layout.tsx
@@ -3,12 +3,12 @@ import HeaderKreator from "~/components/layout/headerkreator";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
-        <div className="flex">
+        <div className="flex h-screen overflow-hidden">
             <SidebarKreator />
-            <div className="flex-1">
+            <div className="flex-1 flex flex-col min-h-0 overflow-hidden">
                 <HeaderKreator />
 
-                <main className="p-6 min-h-screen">
+                <main className="flex-1 overflow-y-auto p-6">
                     {children}
                 </main>
             </div>

fd1e2f52cbfa7d5566838e883b509f02f06c6528
luputer <m.saidibjm4@gmail.com>
2026-05-01 14:47:48 +0800

    debug

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,3 +3,6 @@ import { handlers } from "~/server/auth";
 export const runtime = "nodejs";
 
 export const { GET, POST } = handlers;
+
+console.log(process.env.AUTH_URL)
+console.log(process.env.NEXTAUTH_URL)

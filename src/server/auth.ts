bddbc509e5d09910b483f963fe6f643333f15e88
luputer <m.saidibjm4@gmail.com>
2026-05-01 15:52:53 +0800

    fix: debug auth

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -46,6 +46,7 @@ const credentialsSchema = z.object({
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   ...authConfig,
+  trustHost: true,
   adapter: PrismaAdapter(db),
   providers: [
     GoogleProvider({

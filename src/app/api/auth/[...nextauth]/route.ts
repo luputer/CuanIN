2733fe58a773062d8916c4d3492991d3e6bb3782
luputer <m.saidibjm4@gmail.com>
2026-05-01 16:24:34 +0800

    fix: udpate webhook:

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,6 +3,3 @@ import { handlers } from "~/server/auth";
 export const runtime = "nodejs";
 
 export const { GET, POST } = handlers;
-
-console.log(process.env.AUTH_URL)
-console.log(process.env.NEXTAUTH_URL)
